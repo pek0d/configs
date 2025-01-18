f11c61cd6b10ac694b9908bb727d68e5c9061a04
Марк Рубцов <onathema@gmail.com>
2025-01-19 00:25:34 +0300

    added font hack nerd and tuned codeium font

--- nvim/lua/custom/plugins/codeium.lua
+++ nvim/lua/custom/plugins/codeium.lua
@@ -2,19 +2,19 @@ return {
 	"Exafunction/codeium.vim",
 	config = function()
 		-- Key bindings
-		vim.g.codeium_no_map_tab = true
-		vim.keymap.set("i", "<C-l>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-j>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-k>", function()
-			return vim.fn["codeium#CycleCompletions"](-1)
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-d>", function()
-			return vim.fn["codeium#Clear"]()
-		end, { expr = true, silent = true })
+		-- vim.g.codeium_no_map_tab = true
+		-- vim.keymap.set("i", "<C-l>", function()
+		-- 	return vim.fn["codeium#Accept"]()
+		-- end, { expr = true, silent = true })
+		-- vim.keymap.set("i", "<C-j>", function()
+		-- 	return vim.fn["codeium#CycleCompletions"](1)
+		-- end, { expr = true, silent = true })
+		-- vim.keymap.set("i", "<C-k>", function()
+		-- 	return vim.fn["codeium#CycleCompletions"](-1)
+		-- end, { expr = true, silent = true })
+		-- vim.keymap.set("i", "<C-d>", function()
+		-- 	return vim.fn["codeium#Clear"]()
+		-- end, { expr = true, silent = true })
 	end,
 }
 -- The line beneath this is called `modeline`. See `:help modeline`
--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -4,57 +4,22 @@ return {
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
+    local lint = require 'lint'
+    lint.linters_by_ft = {
+      markdown = { 'markdownlint' },
+    }
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
 
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
+      -- try_lint without arguments runs the linters defined in `linters_by_ft`
+      -- for the current filetype
+      require("lint").try_lint()
 
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          -- Only run the linter in buffers that you can modify in order to
-          -- avoid superfluous noise, notably within the handy LSP pop-ups that
-          -- describe the hovered symbol using Markdown.
-          if vim.opt_local.modifiable:get() then
-            lint.try_lint()
-          end
-        end,
-      })
+      -- You can call `try_lint` with a linter name or a list of names to always
+      -- run specific linters, independent of the `linters_by_ft` configuration
+      require("lint").try_lint("cspell")
+      end,
+    })
     end,
   },
 }
